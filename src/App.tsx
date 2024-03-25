dbac6a459a7ebdf738ad7aaa65c9456d8511465d
Reina <kyejin918@gmail.com>
2024-03-25 14:13:28 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import MainPage from "./pages/MainPage";
+import QuestionPage from "./pages/QuestionPage";
+import ResultPage from "./pages/ResultPage";
+
+function App(): React.ReactElement {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/question" element={<QuestionPage />} />
+        <Route path="/result" element={<ResultPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
